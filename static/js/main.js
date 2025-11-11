43eab46f437c1abffb648c715963c582839c98bc
Ayano Sasakido <20136819@tafe.wa.edu.au>
2025-11-11 12:08:31 +0800

    feat: add play/pause button

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,8 @@
 const input = document.getElementById("videoInput");
 const video = document.getElementById("videoPlayer");
 const source = document.getElementById("videoSource");
+const start_btn = document.getElementById("videoStart")
+const current_time = document.getElementById("")
 
 input.addEventListener("change", e => {
     const file = input.files[0];
@@ -10,5 +12,17 @@ input.addEventListener("change", e => {
         source.src = url;
         video.load();
         video.play();
+        start_btn.textContent = "Pause a video"
     }
+});
+
+start_btn.addEventListener("click", () =>{
+    if(video.play){
+        video.pause();
+        start_btn.textContent = "Play a video"
+    }else if (video.pause){
+        video.play();
+        start_btn.textContent = "Pause a video"
+    }
+
 });
